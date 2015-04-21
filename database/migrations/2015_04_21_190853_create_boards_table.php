e5e67ee3a5c39e49c3095aee5e6ae1ba314e6ea0
8n-tech <root@larachan.larachan.org>
2015-04-21 15:48:50 -0400

    #1 - Rebased Laravel. #3 - Added board migration.

---
+++ database/migrations/2015_04_21_190853_create_boards_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoardsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('boards', function(Blueprint $table)
+		{
+			$table->string('uri', 32)->unique();
+			$table->string('title', 256);
+			$table->string('description', 256)->default(NULL)->nullable();
+			$table->timestamp('created_at');
+			$table->integer('created_by')->unsigned();
+			$table->integer('operated_by')->unsigned();
+			
+			$table->primary('uri');
+			$table->foreign('created_by')->references('id')->on('users');
+			$table->foreign('operated_by')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('boards');
+	}
+
+}
