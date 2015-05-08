14d1f3d81c65f3fabc9d4e8d3752a993720e06c1
8n-tech <root@larachan.larachan.org>
2015-05-08 19:37:42 -0400

    #18 Improved post form.

--- resources/views/content/forms/post.blade.php
+++ resources/views/content/forms/post.blade.php
@@ -2,35 +2,24 @@
 	<input type="hidden" name="_token" value="{{ csrf_token() }}" />
 	
 	<fieldset class="form-fields">
-		<legend class="form-legend">{{{ $reply_to ? "Reply" : "Create a Thread" }}}</legend>
+		<legend class="form-legend">{{{ $reply_to ? "Reply" : "Create Thread" }}}</legend>
 		
-		<div class="field row-subject">
-			<label class="field-label" for="subject">
-				Subject
-				<span class="field-validation"></span>
-			</label>
+		<div class="field row-subject label-inline">
 			<input class="field-control" id="subject" name="subject" type="text" maxlength="255" value="{{ old('subject') }}" />
+			<label class="field-label" for="subject">Subject</label>
 		</div>
 		
-		<div class="field row-author">
-			<label class="field-label" for="author">
-				Name
-				<span class="field-validation"></span>
-			</label>
+		<div class="field row-author label-inline">
 			<input class="field-control" id="author" name="author" type="text" maxlength="255" value="{{ old('author') }}" />
+			<label class="field-label" for="author">Name</label>
 		</div>
 		
-		<div class="field row-email">
-			<label class="field-label" for="email">
-				Email
-				<span class="field-validation"></span>
-			</label>
+		<div class="field row-email label-inline">
 			<input class="field-control" id="email" name="email" type="text" maxlength="255" value="{{ old('email') }}" />
+			<label class="field-label" for="email">Email</label>
 		</div>
 		
 		<div class="field row-post">
-			<label class="field-label" for="body">Post</label>
-				<span class="field-validation"></span>
 			<textarea class="field-control" id="body" name="body" type="text">{{ old('body') }}</textarea>
 		</div>
 		
@@ -43,7 +32,7 @@
 		</div>
 		
 		<div class="field row-submit">
-			<button type="submit" class="field-submit">Submit {{{ $reply_to ? "Reply" : "Thread" }}}</button>
+			<button type="submit" class="field-submit">{{{ $reply_to ? "Post Reply" : "Create Thread" }}}</button>
 		</div>
 	</fieldset>
 </form>
