0dd1f132033f3bbd1532e459c1a1f345fd9bb1c3
8n-tech <root@larachan.larachan.org>
2015-05-08 14:25:07 -0400

    #17

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+<!--
 <div class="grid-container">
 	<section class="grid-80 push-20" id="site-warning">
 		Warning: Some boards on this site might contain content of an adult or offensive nature.<wbr />
@@ -13,6 +14,7 @@
 		In the interest of free speech, only content that directly violates the DMCA or other US laws is deleted.<wbr />
 	</section>
 </div>
+-->
 
 <main id="frontpage">
 	<div class="grid-container">
@@ -47,9 +49,65 @@
 	</div>
 	
 	<div class="grid-container">
-		<section class="grid-100" id="site-boards">
-			@include('widgets.boardsearch')
-		</section>
+		<div class="smooth-box">
+			@include('widgets.messages', [ 'messages' => [ "Larachan's fundraiser has not officially started. Please send feedback for these pages to josh@larachan.org"] ])
+			
+			<section id="site-intro">
+				<h2>Introduction to Larachan</h2>
+				<p>
+					<em>Larachan</em> is an open-source imageboard software suite built on the <a href="//laravel.com">Laravel</a> framework in PHP.
+					It is being built with the intention of superceding all currently available imageboard applications,
+					inspired by my own long time use of imageboards and general low expectations for existing options.
+					My two large contributions with the <a href="https://github.com/ctrlcctrlv/infinity">Infinity Development Group</a> gave rise to the concept of starting from scratch.
+				</p>
+				<p>
+					The ultimate problem with these older codebases is their lack of direction.
+					New, shiny features have to be built around existing flaws.
+					When an inflatable springs a leak and you only apply a patch, the sides of the patch will also start to leak.
+					Without adequaely addressing the problem, eventually end up with a large mass that doesn't actually improve the feature.
+					This is where we are at now.
+				</p>
+				<p>
+					I am someone with 4 years of professional experience in web development, and twice as many years in hobby work.
+					Having been in teams and seen the long-term problems of code complications first hand,
+					I know a complete rewrite with a proper foundation is the only real solution to the problem.
+					The limited numbers of individuals working on separate code bases do not have the resources to slowly reform thousands of lines of code.
+					Without a framework, the state of affairs will continue to deterroriate as more is done by more developers..
+				</p>
+				<p>
+					<strong><a href="{{{url('contribute')}}}">Help me build Larachan, and help the imageboard communities go beyond.</a></strong>
+				</p>
+			</section>
+			
+			<section id="site-faq">
+				<h2>Frequently Asked Questions</h2>
+				
+				<h3>Why should I donate?</h3>
+				<p>
+					If you are even a casual imageboard user, you've probably had a moment where you wanted something to work differently or better.
+					A lot of "easy" additions, like password-protected boards, are not possible because of <em>massive, all-encompassing, fundamental flaws</em> in the software.
+					I do not want to see imageboards go the way of BBS and Usenet, and neither should you.
+					Without a reliable codebase and a developer who can afford to maintain and direct it, these problems will not go away and, at best, will stay the same.
+				</p>
+				
+				<h3>Why did you build your own donation system? Why build it first thing?</h3>
+				<p>
+					Larachan's donation form was developed with Laravel's Stripe integration <em>specifically</em> to avoid Patreon, GoFundMe, Grattipay, and Kickstarter.
+				</p>
+				<p>
+					Users of particular imageboards may be aware that those services are unfaithful to their patrons, despite taking a gratuitous 5 to 10% out of donations.
+					They will kick anyone off their programs for any reason, or no reason at all.
+					In the interest of webmasters, I've built a custom cashier system that can later be converted into a donation form for any website running Larachan.
+					That way, only Stripe can cut funding to the website, and reliance on meddling, self-serving middleman can be avoided entirely.
+				</p>
+				
+				<h3>I do not like this theme / it's too far from what I'm used to.</h3>
+				<p>
+					The website is being built with themeability in mind. The comforting blue gradient will return for those who want it.
+				</p>
+				
+			</section>
+		</div>
 	</div>
 </main>
 @endsection
