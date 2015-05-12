5f34b6feb4c0d69a471ae6d53243a8f68623bcef
8n-tech <root@larachan.larachan.org>
2015-05-12 03:16:32 -0400

    #24 Added basic seeding.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,10 +14,50 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 		
+		$this->call('UserSeeder');
 		$this->call('BoardSeeder');
 	}
 
 }
+
+
+use App\User;
+
+class UserSeeder extends Seeder {
+	
+	protected static $potential_pass_words = [
+		"alarmed", "blue",   "cheap", "digital", // 4
+		"eminent", "free",   "gross", "hairy",   // 8
+		"illegal", "jolly",  "lazy",  "minor",   // 12
+		"nimble",  "perky",  "quiet", "real",    // 16
+		"stiff",   "tragic", "ugly",  "vital",   // 20
+		"wet",     "yellow", "zesty", "key",     // 24
+		"open",    "xeno", // 26
+	];
+	
+	public function run()
+	{
+		$this->command->info('Creating first user ...');
+		
+		// Generate a password.
+		$password = [];
+		for ($i = 0; $i < 4; ++$i)
+		{
+			$password[] = static::$potential_pass_words[array_rand(static::$potential_pass_words)];
+		}
+		$password = implode($password, " ");
+		
+		// Create the user.
+		$user = User::firstOrCreate([
+			'id'       => 1,
+			'username' => "Admin",
+			'password' => bcrypt($password),
+		]);
+		
+		$this->command->info("User \"Admin\" has been created with the following password: {$password}");
+	}
+}
+
 
 use App\Board;
 
@@ -25,16 +65,16 @@ class BoardSeeder extends Seeder {
 	
 	public function run()
 	{
-		$this->command->info('Attempting to seed the /test/ first board.');
+		$this->command->info('Creating first test board ...');
 		
 		$board = Board::firstOrCreate([
-				'uri' => "test",
-				'title' => "Test",
+				'uri'         => "test",
+				'title'       => "Test",
 				'description' => "Discover software features on your own",
-				'created_by' => 1,
+				'created_by'  => 1,
 				'operated_by' => 1,
 			]);
 		
-		$this->command->info('Board exists now. Accessible at /test/.');
+		$this->command->info("Board exists now. Accessible at /test/.");
 	}
 }
