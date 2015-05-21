e366b14a4390912069aef2e5f258205cbb70378c
8n-tech <root@larachan.larachan.org>
2015-05-21 09:31:20 -0400

    #40 Missed a 0 somewhere.

--- resources/views/widgets/devtimer.blade.php
+++ resources/views/widgets/devtimer.blade.php
@@ -3,6 +3,6 @@
 		<a id="contribute-shekel"></a>
 		<h3 class="shekel-title">I can afford to work ...</h3>
 		<blockquote class="shekel-timer" title="This is on the assumption of a 40 hour work week.">{{{ $devTimer }}} </blockquote>
-		<p class="shekel-oyvey">... thanks to ${{ number_format($donations / 10) }} donated by generous contributors.</p>
+		<p class="shekel-oyvey">... thanks to ${{ number_format($donations / 100) }} donated by generous contributors.</p>
 	</div>
 </section>
