4a80cb47448c0607e87109573ea0acbcfc803bc2
jaw-sh <josh@jaw.sh>
2015-07-19 13:30:25 -0400

    #72 Removed post form on catalog view.

--- resources/views/content/catalog.blade.php
+++ resources/views/content/catalog.blade.php
@@ -2,13 +2,6 @@
 
 @section('content')
 <main class="board-index index-catalog">
-	<section class="index-form">
-		@include( $c->template('board.post.form'), [
-			'board'   => $board,
-			'actions' => [ $reply_to ? "reply" : "thread" ],
-		])
-	</section>
-	
 	<section class="index-threads static">
 		<ul class="thread-list">
 			@foreach ($posts as $thread)
