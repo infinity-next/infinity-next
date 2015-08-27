091eec8c549397c27ddde26e02d621020f7ae0d5
jaw-sh <josh@jaw.sh>
2015-08-27 14:36:21 -0400

    #105 Added migration for IP types.

--- database/migrations/2015_08_27_075942_post_ip_binary.php
+++ database/migrations/2015_08_27_075942_post_ip_binary.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Ban;
+use App\Post;
+use App\Report;
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -14,17 +18,69 @@ class PostIpBinary extends Migration
 	{
 		Schema::table('bans', function(Blueprint $table)
 		{
-			// ban_ip
+			$table->binary('ban_ip_end')->after('ban_ip');
+			$table->binary('ban_ip_start')->after('ban_ip');
 		});
-		
 		Schema::table('posts', function(Blueprint $table)
 		{
-			// author_ip
+			$table->binary('author_ip_bin')->nullable()->after('author_ip');
 		});
-		
 		Schema::table('reports', function(Blueprint $table)
 		{
-			// ip
+			$table->binary('reporter_ip')->after('ip');
+		});
+		
+		
+		Ban::chunk(100, function($bans)
+		{
+			foreach ($bans as $ban)
+			{
+				$ban->ban_ip_start = inet_pton($ban->ban_ip);
+				$ban->ban_ip_end   = inet_pton($ban->ban_ip);
+				$ban->save();
+			}
+		});
+		
+		Post::withTrashed()->chunk(100, function($posts)
+		{
+			foreach ($posts as $post)
+			{
+				$post->author_ip_bin = null;
+				
+				if (!is_null($post->author_ip))
+				{
+					$post->author_ip_bin = inet_pton($post->author_ip);
+				}
+				
+				$post->save();
+			}
+		});
+		
+		Report::chunk(100, function($reports)
+		{
+			foreach ($reports as $report)
+			{
+				$report->reporter_ip = inet_pton($report->ip);
+				$report->save();
+			}
+		});
+		
+		
+		Schema::table('bans', function(Blueprint $table)
+		{
+			$table->dropColumn('ban_ip');
+		});
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->dropColumn('author_ip');
+		});
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->renameColumn('author_ip_bin', 'author_ip');
+		});
+		Schema::table('reports', function(Blueprint $table)
+		{
+			$table->dropColumn('ip');
 		});
 	}
 	
@@ -35,6 +91,70 @@ class PostIpBinary extends Migration
 	 */
 	public function down()
 	{
-		//
+		Schema::table('bans', function(Blueprint $table)
+		{
+			$table->string('ban_ip', 46)->after('ban_ip_end');
+		});
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->string('author_ip_string', 46)->after('author_ip');
+		});
+		Schema::table('reports', function(Blueprint $table)
+		{
+			$table->string('ip', 46)->after('reporter_ip');
+		});
+		
+		
+		Ban::chunk(100, function($bans)
+		{
+			foreach ($bans as $ban)
+			{
+				$ban->ban_ip_start = inet_ntop($ban->ban_ip);
+				$ban->ban_ip_end   = inet_ntop($ban->ban_ip);
+				$ban->save();
+			}
+		});
+		
+		Post::withTrashed()->chunk(100, function($posts)
+		{
+			foreach ($posts as $post)
+			{
+				$post->author_ip_string = null;
+				
+				if (!is_null($post->author_ip))
+				{
+					$post->author_ip_string = inet_ntop($post->author_ip);
+				}
+				
+				$post->save();
+			}
+		});
+		
+		Report::chunk(100, function($reports)
+		{
+			foreach ($reports as $report)
+			{
+				$report->ip = inet_ntop($report->reporter_ip);
+				$report->save();
+			}
+		});
+		
+		
+		Schema::table('bans', function(Blueprint $table)
+		{
+			$table->dropColumn('ban_ip_start', 'ban_ip_end');
+		});
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->dropColumn('author_ip');
+		});
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->renameColumn('author_ip_string', 'author_ip');
+		});
+		Schema::table('reports', function(Blueprint $table)
+		{
+			$table->dropColumn('reporter_ip');
+		});
 	}
 }
