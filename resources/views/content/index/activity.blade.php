8f2f36c40baee202bece8d91cf03cdb2975f3105
jaw-sh <josh@jaw.sh>
2015-09-19 19:28:54 -0400

    #117 Removed silly text.

--- resources/views/content/index/activity.blade.php
+++ resources/views/content/index/activity.blade.php
@@ -1,8 +1,6 @@
 <div class="grid-container">
 	@include('widgets.messages')
 	
-	UNDER CONSTRUCTION :---DDDDD
-	
 	@include('content.index.sections.recent_images')
 	
 	@include('content.index.sections.recent_posts')
