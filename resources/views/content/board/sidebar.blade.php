097b0d65c92a019c7816efa42c3e45dc809cf1f0
jaw-sh <josh@jaw.sh>
2015-11-25 16:06:51 -0500

    Fixed critical error in overboard.

--- resources/views/content/board/sidebar.blade.php
+++ resources/views/content/board/sidebar.blade.php
@@ -1,7 +1,9 @@
 <aside class="index-sidebar">
+	@if (isset($board))
 	<div class="sidebar-content">
 		{!! $board->getSidebarContent() !!}
 	</div>
+	@endif
 	
 	@include('widgets.ads.board_bottom_right')
 </aside>
