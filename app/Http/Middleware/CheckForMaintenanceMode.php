d5198300689047b0239d19b4af2a6eb1423255ca
jaw-sh <josh@jaw.sh>
2015-11-27 01:28:48 -0500

    Added missing middleware.

---
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -0,0 +1,33 @@
+<?php namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as MaintenanceMiddleware;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Closure;
+use Request;
+
+class CheckForMaintenanceMode extends MaintenanceMiddleware
+{
+	
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		if ($this->app->isDownForMaintenance())
+		{
+			$admins = explode(",", (string) env('APP_ADMIN_IP'));
+			
+			if (!is_array($admins) || !count($admins) || !in_array(Request::ip(), $admins))
+			{
+				throw new HttpException(503);
+			}
+		}
+		
+		return $next($request);
+	}
+	
+}
