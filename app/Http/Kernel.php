3f36dbb78ad64769f0838c41155d37d3512dd648
jaw-sh <josh@jaw.sh>
2015-11-27 01:27:33 -0500

    Added IP bypass to maintenance mode.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends HttpKernel {
 	 * @var array
 	 */
 	protected $middleware = [
-		'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
+		'App\Http\Middleware\CheckForMaintenanceMode',
 		'Illuminate\Cookie\Middleware\EncryptCookies',
 		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
 		'Illuminate\Session\Middleware\StartSession',
