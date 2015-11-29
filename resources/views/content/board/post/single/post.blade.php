aac9461a25c5ca838cf9572563c245a31e2e1e69
jaw-sh <josh@jaw.sh>
2015-11-29 03:20:43 -0500

    Added attachment data to the post.

--- resources/views/content/board/post/single/post.blade.php
+++ resources/views/content/board/post/single/post.blade.php
@@ -1,6 +1,6 @@
 @spaceless
 @if ($post->hasBody())
-<blockquote class="post ugc">
+<blockquote class="post ugc attachment-count-{{ count($post->attachments) }} {{ count($post->attachments) > 1 ? "attachments-multi" : count($post->attachments) > 0 ? "attachments-single" : "attachments-none" }}">
 	{!! $post->getBodyFormatted() !!}
 </blockquote>
 @endif
