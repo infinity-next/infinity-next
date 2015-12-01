9fe3dfddf7280483813be236ce64574af6b920e5
jaw-sh <josh@jaw.sh>
2015-12-01 13:57:19 -0500

    New validator service provider.

---
+++ app/Providers/ValidationExtensionServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace App\Providers;
+
+use App\Validators\EncodingValidator;
+use App\Validators\FileValidator;
+use Illuminate\Support\ServiceProvider;
+use Validator;
+
+class ValidationExtensionServiceProvider extends ServiceProvider
+{
+	
+	public function boot()
+	{
+		$this->registerValidationRules($this->app['validator']);
+	}
+	
+	protected function registerValidationRules($validator)
+	{
+		$validator->extend('encoding',       'App\Validators\EncodingValidator@validateEncoding');
+		$validator->extend('md5',            'App\Validators\FileValidator@validateMd5');
+		$validator->extend('file_name',      'App\Validators\FileValidator@validateFileName');
+		$validator->extend('file_integrity', 'App\Validators\FileValidator@validateFileIntegrity');
+		$validator->extend('file_new',       'App\Validators\FileValidator@validateFileNew');
+		$validator->extend('file_old',       'App\Validators\FileValidator@validateFileOld');
+	}
+	
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+	
+}
