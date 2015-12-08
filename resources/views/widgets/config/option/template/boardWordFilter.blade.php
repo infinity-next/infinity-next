235ef22b7af51d130a83d705a46d833c5f2ae3c4
jaw-sh <josh@jaw.sh>
2015-12-07 23:00:02 -0500

    Added missing config file.

---
+++ resources/views/widgets/config/option/template/boardWordFilter.blade.php
@@ -0,0 +1,54 @@
+<dl class="option option-{{{ $option_name }}}">
+	<dt class="option-term">
+		{!! Form::label(
+			$option_name,
+			trans("config.option.{$option_name}"),
+			[
+				'class' => "field-label",
+		]) !!}
+		@include('widgets.config.lock')
+	</dt>
+	<dd class="option-definition">
+		<ul class="option-list">
+			@set('option_value', $board->getWordfilters())
+			
+			@foreach ($option_value as $option_find => $option_replace)
+			<li class="option-item">
+				{!! Form::text(
+					$option_name . "[find][]",
+					$option_find,
+					[
+						'class'     => "field-control",
+						isset($board) && isset($option) && !$user->canEditSetting($board, $option) ? 'disabled' : 'data-enabled',
+				]) !!}
+				{!! Form::text(
+					$option_name . "[replace][]",
+					$option_replace,
+					[
+						'class'     => "field-control",
+						isset($board) && isset($option) && !$user->canEditSetting($board, $option) ? 'disabled' : 'data-enabled',
+				]) !!}
+			</li>
+			@endforeach
+			
+			@if (!$option->isLocked())
+			<li class="option-item option-item-template">
+				{!! Form::text(
+					$option_name . "[find][]",
+					null,
+					[
+						'class'     => "field-control",
+				]) !!}
+				{!! Form::text(
+					$option_name . "[replace][]",
+					null,
+					[
+						'class'     => "field-control",
+				]) !!}
+			</li>
+			@endif
+		</ul>
+		
+		@include('widgets.config.helper')
+	</dd>
+</dl>
