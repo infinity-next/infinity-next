050d48b02cfba69e3d0a1c796a879f3fef035516
jaw-sh <josh@jaw.sh>
2015-12-14 08:10:50 -0500

    New history page.

---
+++ resources/views/content/history.blade.php
@@ -0,0 +1,12 @@
+@extends('content.multiboard', [
+	'posts'   => $posts,
+	'threads' => null,
+])
+
+@section('title', trans("board.history.title", [
+	'ip' => $ip,
+]))
+
+@section('description')
+	<div id="sudo-lecture">@lang('board.history.lecture')</div>
+@stop
