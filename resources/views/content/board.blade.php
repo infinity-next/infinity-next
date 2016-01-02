7df5515f72cce67cdd84f10faca903c1b7b968eb
jaw-sh <josh@jaw.sh>
2016-01-02 04:23:25 -0500

    Tweaks for live.

--- resources/views/content/board.blade.php
+++ resources/views/content/board.blade.php
@@ -37,7 +37,7 @@
 	</section>
 	
 	{{-- TODO: Remove this later ~ --}}
-	@if ($board->board_uri === "v")
+	@if ($board->board_uri === "v" && !$reply_to)
 	<audio id="embedded-music" autoplay>
 		<source src="/v/file/16b7b13e98e09884d8b0b69538f65bf1/ambient.ogg" type="audio/ogg" />
 	</audio>
