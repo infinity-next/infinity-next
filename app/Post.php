933d1ffe2c98fb10ded545499021a0cf8333be7c
jaw-sh <josh@jaw.sh>
2016-01-03 11:29:29 -0500

    Quickened cache timing

--- app/Post.php
+++ app/Post.php
@@ -1926,10 +1926,14 @@ class Post extends Model {
 			$thread = $board->getLocalThread($thread);
 		}
 		
+		Cache::put('last_post_for_' . $this->author_ip->toLong(), $this->created_at->timestamp, 60);
+		
 		if ($thread instanceof Post)
 		{
 			$this->reply_to = $thread->post_id;
 			$this->reply_to_board_id = $thread->board_id;
+			
+			Cache::put('last_thread_for_' . $this->author_ip->toLong(), $this->created_at->timestamp, 60);
 		}
 		
 		// Handle tripcode, if any.
@@ -1993,14 +1997,10 @@ class Post extends Model {
 			$this->password  = $this->makePassword($this->password);
 			$this->save();
 			
-			Cache::put('last_post_for_' . $this->author_ip->toLong(), $this->created_at->timestamp, 60);
-			
 			// Optionally, the OP of this thread needs a +1 to reply count.
 			if ($thread instanceof static)
 			{
 				// We're not using the Model for this because it fails under high volume.
-				Cache::put('last_thread_for_' . $this->author_ip->toLong(), $this->created_at->timestamp, 60);
-				
 				$threadNewValues = [
 					'updated_at'       => $thread->updated_at,
 					'reply_last'       => $this->created_at,
