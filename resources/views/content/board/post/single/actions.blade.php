6040c4e4b9aa7ecc6174c49f5082ed99f0b7c5bd
Joshua Moon <josh@jaw.sh>
2016-05-13 05:52:00 -0500

    Fixed issue where actions could cause a 500 error on old posts.

--- resources/views/content/board/post/single/actions.blade.php
+++ resources/views/content/board/post/single/actions.blade.php
@@ -4,10 +4,10 @@
 	@set('postActions', false)
 	<div class="post-action-tab action-tab-actions" data-no-instant>
 		<span class="post-action-label post-action-open"><i class="fa fa-bars"></i><span class="post-action-text">@lang('board.action.open')</span></span>
-		
+
 		<ul class="post-action-groups">
 			<li class="post-action-group">
-				
+
 				<!-- Board specific content management actions -->
 				<ul class="post-actions">
 					@if ($post->canReport($user))
@@ -16,36 +16,36 @@
 						<a class="post-action-link action-link-report" href="{!! $post->url("report") !!}">@lang('board.action.report')</a>
 					</li>
 					@endif
-					
+
 					@if ($post->canReportGlobally($user))
 					@set('postActions', true)
 					<li class="post-action">
 						<a class="post-action-link action-link-report-global" href="{!! $post->url("report/global") !!}">@lang('board.action.report_global')</a>
 					</li>
 					@endif
-					
-					@if (isset($details['author_ip']) && !is_null($details['author_ip']))
+
+					@if (isset($details['author_ip']) and !is_null($details['author_ip']))
 					@if ($user->canViewHistory($post))
 					@set('postActions', true)
 					<li class="post-action">
 						<a class="post-action-link action-link-report" href="{!! $board->getURL('history/' . $details['board_id']) !!}">@lang('board.action.history', [ 'board_uri' => $details['board_uri'] ])</a>
 					</li>
 					@endif
-					@if ($user->canViewGlobalHistory())
+					@if ($user->canViewGlobalHistory() and $details['author_ip'])
 					@set('postActions', true)
 					<li class="post-action">
 						<a class="post-action-link action-link-report" href="{!! url('cp/history/' . $details['author_ip']->toText()) !!}">@lang('board.action.history_global')</a>
 					</li>
 					@endif
 					@endif
-					
+
 					@if ($post->canEdit($user))
 					@set('postActions', true)
 					<li class="post-action">
 						<a class="post-action-link action-link-edit" href="{!! $post->url("edit") !!}">@lang('board.action.edit')</a>
 					</li>
 					@endif
-					
+
 					@if ($post->canSticky($user))
 					@set('postActions', true)
 					<li class="post-action">
@@ -56,7 +56,7 @@
 						@endif
 					</li>
 					@endif
-					
+
 					@if ($post->canLock($user))
 					@set('postActions', true)
 					<li class="post-action">
@@ -67,7 +67,7 @@
 						@endif
 					</li>
 					@endif
-					
+
 					@if ($post->canBumplock($user))
 					@set('postActions', true)
 					<li class="post-action">
@@ -78,9 +78,9 @@
 						@endif
 					</li>
 					@endif
-					
+
 				</ul>
-				
+
 				<!-- Broad sweeping user & board actions -->
 				<ul class="post-actions">
 					@if ($board->canBan($user))
@@ -89,46 +89,46 @@
 						<a class="post-action-link action-link-ban" href="{!! $post->url("mod/ban") !!}">@lang('board.action.ban')</a>
 					</li>
 					@endif
-					
+
 					@if ($post->canDelete($user))
 					@set('postActions', true)
 						<li class="post-action">
 							<a class="post-action-link action-link-delete" href="{!! $post->url("mod/delete") !!}">@lang('board.action.delete')</a>
 						</li>
-						
+
 						@if ($board->canDelete($user))
 						<li class="post-action">
 							<a class="post-action-link action-link-delete-all" href="{!! $post->url("mod/delete/all") !!}">@lang('board.action.delete_board')</a>
 						</li>
 						@endif
-						
+
 						@if ($board->canBan($user))
 						<li class="post-action">
 							<a class="post-action-link action-link-ban-delete" href="{!! $post->url("mod/ban/delete") !!}">@lang('board.action.ban_delete')</a>
 						</li>
-						
+
 						<li class="post-action">
 							<a class="post-action-link action-link-ban-delete-all" href="{!! $post->url("mod/ban/delete/all") !!}">@lang('board.action.ban_delete_board')</a>
 						</li>
 						@endif
 					@endif
 				</ul>
-				
+
 				<ul class="post-actions">
 					@if ($user->canFeatureGlobally($post))
 					@set('postActions', true)
 						<li class="post-action">
 							<a class="post-action-link action-link-feature-global" href="{!! $post->url("feature/global") !!}">@lang('board.action.feature_global')</a>
 						</li>
-						
+
 					@endif
-					
+
 					@if ($user->canDeleteGlobally())
 					@set('postActions', true)
 						<li class="post-action">
 							<a class="post-action-link action-link-delete-global" href="{!! $post->url("mod/delete/global") !!}">@lang('board.action.delete_global')</a>
 						</li>
-						
+
 						@if ($user->canBanGlobally())
 						<li class="post-action">
 							<a class="post-action-link action-link-ban-delete-global" href="{!! $post->url("mod/ban/delete/global") !!}">@lang('board.action.ban_delete_global')</a>
@@ -139,13 +139,13 @@
 			</li>
 		</ul>
 	</div>
-	
+
 	@if ($postActions)
 		@overwrite
 		@yield('post-actions')
 	@else
 		@stop
 	@endif
-	
+
 </div>
 @endspaceless
