aae58810b9dd9361161a789c0c2e6a61d879da7a
Joshua Moon <josh@jaw.sh>
2016-05-21 20:12:57 -0500

    New attachment layout.

--- resources/views/content/board/post/single/attachments.blade.php
+++ resources/views/content/board/post/single/attachments.blade.php
@@ -4,21 +4,49 @@
 	@foreach ($post->attachments as $attachment)
 	<li class="post-attachment">
 		@if (!isset($catalog) || !$catalog)
-		<div class="attachment-side-actions">
-			@if ($attachment->isSpoiler())
-			<a href="{{ $attachment->getUnspoilerURL($board) }}" class="attachment-action attachment-unspoiler" title="@lang('board.field.unspoiler')" data-no-instant>
-				<i class="fa fa-question"></i>&nbsp;@lang('board.field.unspoiler')
-			</a>
-			@else
-			<a href="{{ $attachment->getSpoilerURL($board) }}" class="attachment-action attachment-spoiler" title="@lang('board.field.spoiler')" data-no-instant>
-				<i class="fa fa-question"></i>&nbsp;@lang('board.field.spoiler')
-			</a>
-			@endif
+		<div class="post-action-bar action-bar-attachments">
+			@section('attachment-actions')
+			@set('attachmentActions', false)
+			<div class="post-action-tab action-tab-actions" data-no-instant>
+				<span class="post-action-label post-action-open">▷<span class="post-action-text">@lang('board.action.open')</span></span>
+				<ul class="post-action-groups">
+					<li class="post-action-group">
+						<ul class="post-actions">
+							@set('attachmentActions', true)
+							@if ($attachment->isSpoiler())
+							<li class="post-action">
+								<a href="{{ $attachment->getUnspoilerURL($board) }}" target="_blank" class="post-action-link attachment-unspoiler" title="@lang('board.field.unspoiler')" data-no-instant>
+									<i class="fa fa-question"></i>&nbsp;@lang('board.field.unspoiler')
+								</a>
+							</li>
+							@else
+							<li class="post-action">
+								<a href="{{ $attachment->getSpoilerURL($board) }}" target="_blank" class="post-action-link attachment-spoiler" title="@lang('board.field.spoiler')" data-no-instant>
+									<i class="fa fa-question"></i>&nbsp;@lang('board.field.spoiler')
+								</a>
+							</li>
+							@endif
 
-			<a href="{{ $attachment->getRemoveURL($board) }}" class="attachment-action attachment-remove" title="@lang('board.field.remove')" data-no-instant>
-				<i class="fa fa-remove"></i>&nbsp;@lang('board.field.remove')
+							<li class="post-action">
+								<a href="{{ $attachment->getRemoveURL($board) }}" target="_blank" class="post-action-link attachment-remove" title="@lang('board.field.remove')" data-no-instant>
+									<i class="fa fa-remove"></i>&nbsp;@lang('board.field.remove')
+								</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			@show
+		</div>
+
+		<div class="attachment-action-group">
+			<a class="attachment-action attachment-download" target="_blank" href="{!! $attachment->getDownloadURL($board) . "?disposition=attachment" !!}" download="{!! $attachment->getDownloadName() !!}">
+				<span class="detail-item detail-download">@lang('board.field.download')</span>
+				<span class="detail-item detail-filesize">{{ $attachment->getHumanFilesize() }}</span>
+				<span class="detail-item detail-filedim" title="{{ $attachment->getFileDimensions() }}">{{ $attachment->getFileDimensions() }}</span>
 			</a>
 		</div>
+
 		<div class="attachment-container">
 			@if ($attachment->isDeleted())
 			<figure class="attachment attachment-deleted">
@@ -32,8 +60,6 @@
 				data-thumb-url="{!! $attachment->getThumbnailURL($board) !!}"
 			>
 				<figure class="attachment attachment-type-{{ $attachment->guessExtension() }} {{ $attachment->getThumbnailClasses() }}" data-widget="lazyimg">
-					{!! $attachment->getThumbnailHTML($board) !!}
-
 					<figcaption class="attachment-details">
 						<p class="attachment-detail">
 							@if ($attachment->pivot->is_spoiler)
@@ -43,17 +69,10 @@
 							@endif
 						</p>
 					</figcaption>
+
+					{!! $attachment->getThumbnailHTML($board) !!}
 				</figure>
 			</a>
-
-			<div class="attachment-action-group">
-				<a class="attachment-action attachment-download" target="_blank" href="{!! $attachment->getDownloadURL($board) . "?disposition=attachment" !!}" download="{!! $attachment->getDownloadName() !!}">
-					<i class="fa fa-download"></i>
-					<span class="detail-item detail-download">@lang('board.field.download')</span>
-					<span class="detail-item detail-filesize">{{ $attachment->getHumanFilesize() }}</span>
-					<span class="detail-item detail-filedim" title="{{ $attachment->getFileDimensions() }}">{{ $attachment->getFileDimensions() }}</span>
-				</a>
-			</div>
 			@endif
 		</div>
 		@else
