36a2e6003ac84d077d58e7d5cde65acfd1aade0b
Joshua Moon <josh@jaw.sh>
2016-05-25 03:02:09 -0500

    Fixed index page.

--- resources/views/content/index/sections/recent_posts.blade.php
+++ resources/views/content/index/sections/recent_posts.blade.php
@@ -12,7 +12,7 @@
 					<li class="recent-post grid-25 tablet-grid-25 mobile-grid-50">
 						<span class="recent-post-bg" style="background-image: {{ $board_icon }};"></span>
 						<a class="recent-post-link @if ($post->isOp()) recent-post-op @endif" href="{{ $post->getURL() }}"></a>
-						<blockquote class="post ugc" dir="{{ $post->getBodyDirection() }}">
+						<blockquote class="post ugc" {!! $post->getBodyDirectionAttr() !!}>
 							{!! $post->getBodyFormatted() !!}
 						</blockquote>
 					</li>
