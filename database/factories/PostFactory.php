bebe011d76b8b060929eea8009edb48bcf16af68
Joshua Moon <josh@jaw.sh>
2020-03-30 22:22:46 -0400

    Post faker

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'board_uri' => $faker->word,
+
+        'bumped_last' => now(),
+        'created_at' => now(),
+        'updated_at' => null,
+
+        'author_ip' $faker->ipv4,
+        'subject' => $faker->sentence,
+        'author' => $faker->name,
+        'email' => $faker->email,
+        'password' => $faker->password,
+
+        'body' => $faker->paragraph(3, true),
+    ];
+});
