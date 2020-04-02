0e4fbf6e9081586e3f30f88f0466e3d5ace9c9b9
Joshua Moon <josh@jaw.sh>
2020-04-02 00:35:15 -0400

    Added crown to featured post.

--- resources/views/content/index/sections/featured_post.blade.php
+++ resources/views/content/index/sections/featured_post.blade.php
@@ -1,11 +1,10 @@
 @if ($featured && $featured instanceof \App\Post)
 <section id="site-featured-post" class="grid-100">
     <div class="smooth-box">
-        <h2 class="index-title">@lang('index.title.featured_post')</h2>
-
         @include('content.board.post', [
             'board' => $featured->board,
             'post'  => $featured,
+            'crown' => true,
         ])
     </div>
 </section>
