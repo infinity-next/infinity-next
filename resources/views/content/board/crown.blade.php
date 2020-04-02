935a829081e55244798c315c5d0f876cfa869d84
Joshua Moon <josh@jaw.sh>
2020-04-02 00:42:50 -0400

    Syntax error.

--- resources/views/content/board/crown.blade.php
+++ resources/views/content/board/crown.blade.php
@@ -2,7 +2,7 @@
     <figure class="crown-figure">
         <img class="crown-image" src="{{ $board->getIconURL() }}" />
         <figcaption class="crown-title">
-            <a href="{!! $featured_board->getUrl() !!}" class="crown-link">/{{ $board->board_uri }}/ - {{ $board->getDisplayName() }}</a>
+            <a href="{!! $board->getUrl() !!}" class="crown-link">/{{ $board->board_uri }}/ - {{ $board->getDisplayName() }}</a>
         </figcaption>
     </figure>
 </div>
