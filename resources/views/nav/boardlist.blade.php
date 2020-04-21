f78327409c1f1cb328d621827eee528663353307
Joshua Moon <josh@jaw.sh>
2020-04-21 07:08:16 -0400

    Some retro boardlist modifications.

--- resources/views/nav/boardlist.blade.php
+++ resources/views/nav/boardlist.blade.php
@@ -1,34 +1,44 @@
 <!-- Yes, this is only here so you can style it back into existance. -->
 <nav class="boardlist" data-instant>
-    <div class="boardlist-row row-links">
-        <ul class="boardlist-categories">
-            <li class="boardlist-category">
-                <ul class="boardlist-items">
+    <span class="boardlist-row row-links">
+        <span class="boardlist-categories">
+            <span class="boardlist-category">
+                <span class="boardlist-items">
                     @foreach ($navLinks as $navItem => $navUrl)
-                    <li class="boardlist-item">
+                    <span class="boardlist-item">
                         <a href="{!! $navUrl !!}" class="boardlist-link">{{ trans("nav.global.{$navItem}") }}</a>
-                    </li>
+                    </span>
                     @endforeach
-                </ul>
-            </li>
-        </ul>
-    </div>
+                </span>
+            </span>
+        </span>
+    </span>
 
     @if ($showBoardList)
-    <div class="boardlist-row row-boards">
-        <ul class="boardlist-categories">
+    <span class="boardlist-row row-boards">
+        <span class="boardlist-categories">
         @foreach ($navBoards as $groupname => $boards)
-            <li class="boardlist-category">
-                <ul class="boardlist-items">
+            <span class="boardlist-category">
+                <span class="boardlist-items">
                     @foreach ($boards as $board)
-                    <li class="boardlist-item">
+                    <span class="boardlist-item">
                         <a href="{!! $board->getUrl() !!}" class="boardlist-link">{!! $board->board_uri !!}</a>
-                    </li>
+                    </span>
                     @endforeach
-                </ul>
-            </li>
+                </span>
+            </span>
         @endforeach
-        </ul>
-    </div>
+        </span>
+    </span>
     @endif
+
+    <span class="boardlist-row row-right">
+        <span class="boardlist-categories">
+            <span class="boardlist-category">
+                <span class="boardlist-items">
+                    <span class="boardlist-link" data-widget="js-config">@lang("nav.global.options")</a>
+                </span>
+            </span>
+        </span>
+    </span>
 </nav>
