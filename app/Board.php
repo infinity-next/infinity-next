d27977f643e7a8be6293dd933037a80498885839
root <root@9chan.org>
2020-04-24 13:11:39 +0000

    Reduced ppx specificity

--- app/Board.php
+++ app/Board.php
@@ -788,7 +788,7 @@ class Board extends Model
 
         if ($stats->count()) {
             $oldest = $stats->sortBy('stats_time')->first();
-            return number_format($stats->sum('counter') / max($oldest->stats_time->diffInHours(), 1), 2);
+            return number_format($stats->sum('counter') / max($oldest->stats_time->diffInHours(), 1), 1);
         }
 
         return 0;
@@ -811,7 +811,7 @@ class Board extends Model
 
         if ($stats->count()) {
             $oldest = $stats->sortBy('stats_time')->first();
-            return number_format($stats->sum('counter') / max($oldest->stats_time->diffInDays(), 1), 2);
+            return number_format($stats->sum('counter') / max($oldest->stats_time->diffInDays(), 1), 0);
         }
 
         return 0;
