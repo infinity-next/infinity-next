7880bc627e863f4d03cbe4a92e32b222c66ebaf2
Joshua Moon <josh@jaw.sh>
2020-04-25 10:20:24 -0400

    Fixed prior commit.

--- app/Http/Controllers/Board/BoardController.php
+++ app/Http/Controllers/Board/BoardController.php
@@ -286,11 +286,13 @@ class BoardController extends Controller
      */
     public function putThread(PostRequest $request, Board $board)
     {
-
+        // lock the connection
+        $connLock = $this->lockConnection();
+        // lock the destination
         $lock = Cache::lock("posting_now_board:{$board->board_uri}", 5);
 
         try {
-            $connLock->block(1);
+            $connLock->block(5);
             $lock->block(5);
 
             try {
