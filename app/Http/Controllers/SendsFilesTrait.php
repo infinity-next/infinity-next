9eac2116a6f43d5c99c54775395fdbed9ac8dcc3
Joshua Moon <josh@jaw.sh>
2020-05-03 15:07:25 -0400

    Revert "410 error for file controller."
    This reverts commit 6c50d989a3309cf2c2d9f3ad32cf22507b6d48cd.

--- app/Http/Controllers/SendsFilesTrait.php
+++ app/Http/Controllers/SendsFilesTrait.php
@@ -47,7 +47,7 @@ trait SendsFilesTrait
             $FileStorage = FileStorage::getHash($hash);
 
             if (is_null($FileStorage)) {
-                return abort(404);
+                return abort(410, "File is gone from storage.");
             }
 
             if (!is_string($filename)) {
@@ -71,117 +71,116 @@ trait SendsFilesTrait
                 $storageExists = Storage::exists($storagePath);
             }
 
+
             if ($storageExists) {
-                return abort(410, "File is gone from storage.");
-            }
+                ini_set('zlib.output_compression', 'Off');
 
-            ini_set('zlib.output_compression', 'Off');
+                $responseSize = filesize($storagePathFull);
+                $responseCode = 200;
+                $responseHeaders = [
+                    'Cache-Control' => "public, max-age={$cacheTime}, pre-check={$cacheTime}",
+                    'Expires' => gmdate(DATE_RFC1123, time() + $cacheTime),
+                    'Last-Modified' => gmdate(DATE_RFC1123, File::lastModified($storagePathFull)),
+                    'Content-Disposition' => Request::input('disposition', 'inline'),
+                    //'Content-Disposition'  => "attachment; filename={$filename}",
+                    'Content-Length' => $responseSize,
+                    'Content-Type' => $FileStorage->mime,
+                    'Filename' => urldecode($filename),
+                ];
 
-            $responseSize = filesize($storagePathFull);
-            $responseCode = 200;
-            $responseHeaders = [
-                'Cache-Control' => "public, max-age={$cacheTime}, pre-check={$cacheTime}",
-                'Expires' => gmdate(DATE_RFC1123, time() + $cacheTime),
-                'Last-Modified' => gmdate(DATE_RFC1123, File::lastModified($storagePathFull)),
-                'Content-Disposition' => Request::input('disposition', 'inline'),
-                //'Content-Disposition'  => "attachment; filename={$filename}",
-                'Content-Length' => $responseSize,
-                'Content-Type' => $FileStorage->mime,
-                'Filename' => urldecode($filename),
-            ];
+                // Determine if we can skip PHP content distribution.
+                // This is hugely important.
+                $xSendFile = false;
 
-            // Determine if we can skip PHP content distribution.
-            // This is hugely important.
-            $xSendFile = false;
-
-            // APACHE
-            // Relies on the mod_xsendfile module.
-            if (function_exists('apache_get_modules') && in_array('mod_xsendfile', apache_get_modules())) {
-                $xSendFile = true;
-                $responseHeaders['X-Sendfile'] = $storagePathFull;
-            }
-            // NGINX
-            elseif (preg_match("/nginx\/1(\.[0-9]+)+/", $_SERVER['SERVER_SOFTWARE'])) {
-                $xSendFile = true;
-                $responseHeaders['X-Accel-Redirect'] = "/{$storagePath}";
-            }
-            // LIGHTTPD
-            elseif (preg_match("/lighttpd\/1(\.[0-9]+)+/", $_SERVER['SERVER_SOFTWARE'])) {
-                $xSendFile = true;
-                $responseHeaders['X-LIGHTTPD-send-file'] = $storagePathFull;
-            }
-
-
-            // Seek Audio and Video files.
-            $responseStart = 0;
-            $responseEnd = $responseSize - 1;
-
-            if ($FileStorage->isVideo()) {
-                $responseHeaders['Accept-Ranges'] = '0-'.($responseSize - 1);
-
-                if (isset($_SERVER['HTTP_RANGE'])) {
-                    list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-
-                    if (strpos($range, ',') !== false) {
-                        return Response::make('Requested Range Not Satisfiable', 416, [
-                            'Content-Range' => "bytes {$responseStart}-{$responseEnd}/{$responseSize}",
-                        ]);
-                    }
-
-                    if ($range == '-') {
-                        $responseStart = $this->size - substr($range, 1);
-                    } else {
-                        $range = explode('-', $range);
-                        $responseStart = $range[0];
-
-                        $responseEnd = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $responseEnd;
-                    }
-
-                    if ($responseStart > $responseEnd || $responseStart > $responseSize - 1 || $responseEnd >= $responseSize) {
-                        return Response::make('Requested Range Not Satisfiable', 416, [
-                            'Content-Range' => "bytes {$responseStart}-{$responseEnd}/{$responseSize}",
-                        ]);
-                    }
-
-                    $responseCode = 206;
-                    $responseHeaders['Content-Length'] = $responseSize - $responseStart;
-                    $responseHeaders['Content-Range'] = "bytes {$responseStart}-{$responseEnd}/{$responseSize}";
-
-                    unset($responseHeaders['Accept-Ranges']);
-                    unset($responseHeaders['Cache-Control']);
-                    unset($responseHeaders['Content-Disposition']);
-                    unset($responseHeaders['Expires']);
+                // APACHE
+                // Relies on the mod_xsendfile module.
+                if (function_exists('apache_get_modules') && in_array('mod_xsendfile', apache_get_modules())) {
+                    $xSendFile = true;
+                    $responseHeaders['X-Sendfile'] = $storagePathFull;
                 }
-            }
+                // NGINX
+                elseif (preg_match("/nginx\/1(\.[0-9]+)+/", $_SERVER['SERVER_SOFTWARE'])) {
+                    $xSendFile = true;
+                    $responseHeaders['X-Accel-Redirect'] = "/{$storagePath}";
+                }
+                // LIGHTTPD
+                elseif (preg_match("/lighttpd\/1(\.[0-9]+)+/", $_SERVER['SERVER_SOFTWARE'])) {
+                    $xSendFile = true;
+                    $responseHeaders['X-LIGHTTPD-send-file'] = $storagePathFull;
+                }
 
 
-            // Are we using the webserver to send files?
-            if ($xSendFile) {
-                // Yes.
-                // Send an empty 200 response with the headers.
-                return Response::make('', $responseCode, $responseHeaders);
-            }
-            else {
-                // No.
-                // Get our hands dirty and stream the file.
-                return Response::make(function () use ($storagePathFull, $responseStart, $responseEnd) {
-                    if (!($responseStream = fopen($storagePathFull, 'rb'))) {
-                        abort(500, 'Could not open requested file.');
+                // Seek Audio and Video files.
+                $responseStart = 0;
+                $responseEnd = $responseSize - 1;
+
+                if ($FileStorage->isVideo()) {
+                    $responseHeaders['Accept-Ranges'] = '0-'.($responseSize - 1);
+
+                    if (isset($_SERVER['HTTP_RANGE'])) {
+                        list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+
+                        if (strpos($range, ',') !== false) {
+                            return Response::make('Requested Range Not Satisfiable', 416, [
+                                'Content-Range' => "bytes {$responseStart}-{$responseEnd}/{$responseSize}",
+                            ]);
+                        }
+
+                        if ($range == '-') {
+                            $responseStart = $this->size - substr($range, 1);
+                        } else {
+                            $range = explode('-', $range);
+                            $responseStart = $range[0];
+
+                            $responseEnd = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $responseEnd;
+                        }
+
+                        if ($responseStart > $responseEnd || $responseStart > $responseSize - 1 || $responseEnd >= $responseSize) {
+                            return Response::make('Requested Range Not Satisfiable', 416, [
+                                'Content-Range' => "bytes {$responseStart}-{$responseEnd}/{$responseSize}",
+                            ]);
+                        }
+
+                        $responseCode = 206;
+                        $responseHeaders['Content-Length'] = $responseSize - $responseStart;
+                        $responseHeaders['Content-Range'] = "bytes {$responseStart}-{$responseEnd}/{$responseSize}";
+
+                        unset($responseHeaders['Accept-Ranges']);
+                        unset($responseHeaders['Cache-Control']);
+                        unset($responseHeaders['Content-Disposition']);
+                        unset($responseHeaders['Expires']);
                     }
+                }
 
-                    if ($responseStart > 0) {
-                        fseek($responseStream, $responseStart);
-                    }
 
-                    $streamCurrent = 0;
+                // Are we using the webserver to send files?
+                if ($xSendFile) {
+                    // Yes.
+                    // Send an empty 200 response with the headers.
+                    return Response::make('', $responseCode, $responseHeaders);
+                }
+                else {
+                    // No.
+                    // Get our hands dirty and stream the file.
+                    return Response::make(function () use ($storagePathFull, $responseStart, $responseEnd) {
+                        if (!($responseStream = fopen($storagePathFull, 'rb'))) {
+                            abort(500, 'Could not open requested file.');
+                        }
 
-                    while (!feof($responseStream) && $streamCurrent < $responseEnd && connection_status() == 0) {
-                        echo fread($responseStream, min(1024 * 16, $responseEnd - $responseStart + 1));
-                        $streamCurrent += 1024 * 16;
-                    }
+                        if ($responseStart > 0) {
+                            fseek($responseStream, $responseStart);
+                        }
 
-                    fclose($responseStream);
-                }, $responseCode, $responseHeaders);
+                        $streamCurrent = 0;
+
+                        while (!feof($responseStream) && $streamCurrent < $responseEnd && connection_status() == 0) {
+                            echo fread($responseStream, min(1024 * 16, $responseEnd - $responseStart + 1));
+                            $streamCurrent += 1024 * 16;
+                        }
+
+                        fclose($responseStream);
+                    }, $responseCode, $responseHeaders);
+                }
             }
         }
 
